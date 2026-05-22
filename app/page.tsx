a24f846149144464a0ffd5fa682d6101f29e9a3a
Niyamul <mdniyamulwalid@gmail.com>
2026-05-22 23:48:39 +0600

    fix app router 404

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       flexDirection: "column"
     }}>
       <h1>NorthMart 🚀</h1>
-      <p>Deployed Successfully</p>
+      <p>Website is Working Now</p>
     </div>
   );
 }
